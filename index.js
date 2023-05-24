6c9914d05e27d30d646a01921da4260a39385bde
Paulo Freitas <paulo.freitas.nt@gmail.com>
2023-05-24 14:03:45 -0300

    Construindo a conexão, schema, modelo e salvando

---
+++ index.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/aula');
+  console.log('Conectado');
+}
+
+const anotacaoSchema = new mongoose.Schema({
+  autor: String,
+  data: { type: Date, default: Date.now },
+  titulo: String,
+  conteudo: String  
+});
+
+const Anotacao = mongoose.model('Anotacao', anotacaoSchema);
+
+const anotacao1 = new Anotacao({
+    autor: "João",
+    data: "2022-12-12",
+    titulo: "Exemplo de anotação",
+    conteudo: "Exemplo de anotação feita em sala"
+});
+
+Anotacao.create(anotacao1).then(console.log('Salvo'));
